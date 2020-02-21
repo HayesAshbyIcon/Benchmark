60638131e63b65218af7cc81d39f4491be8ae8cd
Hayes Ashby <hayes.ashby@gmail.com>
2020-02-21 13:04:54 -0700

    Fixed the bug with other devices not properly parsing the xml

--- TapTool/tap_id.sh
+++ TapTool/tap_id.sh
@@ -1,17 +1,20 @@
     #Set the First Argument of the script to the id to tap
     id=$1
-    echo "Tapping element: " $id
+
     #To find future IDs go to the screen in which the id is present then use the commands:
     #"adb shell uiautomator dump"
     #"adb shell /sdcard/window_dump.xml" 
     #And locate via the Text on the button or other means
 
     #Big Complicated thing Thats doing a lot https://stackoverflow.com/questions/18924968/using-adb-to-access-a-particular-ui-control-on-the-screen
-    if adb exec-out uiautomator dump /dev/tty | sed 's/UI hierchary dumped to: \/dev\/tty//' | sed 'N;s/Usage: don'\''t use this (cf dexopt usage).\n//' | xpath 'string(//node[@resource-id="'"$id"'"]/@bounds)' 2> /dev/null; 
+    if adb shell echo "Tapping element: " $id 
     then 
 
-        coords=$(adb exec-out uiautomator dump /dev/tty | sed 's/UI hierchary dumped to: \/dev\/tty//' | sed 'N;s/Usage: don'\''t use this (cf dexopt usage).\n//' | xpath 'string(//node[@resource-id="'"$id"'"]/@bounds)' 2> /dev/null)
+        adb shell uiautomator dump
+        adb pull /sdcard/window_dump.xml
+        coords=$(xpath window_dump.xml //node[@resource-id="'$id'"]/@bounds)
 
+        
         #We are creating extra variables here but its if for future development we would like not to tap the top most left pixel
         leftuppercoords=$(echo "$coords" | cut -d "[" -f 2 | cut -d "]" -f 1)
         #X and Y
--- debugging.sh
+++ debugging.sh
@@ -1,1 +1,9 @@
 #Used For Creating new scripts to implement into the runner
+
+if adb shell echo "Starting Debugging Test"
+then 
+
+else
+    #Couldn't find Id
+        echo "Couldn't find Device"
+fi
